eeecb7c65db0dadfc2f9503704a450173f2a314e
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-01-09 14:17:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var request = new XMLHttpRequest();
+
+request.open("POST", "https://www.spotternetwork.org/positions");
+
+request.setRequestHeader("Content-Type", "application/json");
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log("Status:", this.status);
+    console.log("Headers:", this.getAllResponseHeaders());
+    console.log("Body:", this.responseText);
+    document.getElementById("myData").innerHTML = this.responseText;
+  }
+};
+
+var body = {
+  id: "APPLICATION-ID"
+};
+
+request.send(JSON.stringify(body));
